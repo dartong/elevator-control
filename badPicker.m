e3a1e8b34a8bd4603d32dd79e7ee1f4ed94170ac
Callie Doyle <Kdoyle11@nd.edu>
2018-04-23 23:07:47 -0400

    badPicker

---
+++ badPicker.m
@@ -0,0 +1,31 @@
+function [carIndex, sums] = badPicker(t, config, cars, call)
+%% Create scoring system, set up basic parameters
+
+num_cars = config.NUM_CARS; 
+sums = zeros(1,num_cars);   
+distanceFracBase = 20;  
+
+
+%% Sum totals
+for iCar = 1:num_cars
+    %% simplify structs
+    % car data
+    currentPos = cars(iCar).y;
+    destinations = cars(iCar).destinations;
+    velocity = cars(iCar).velocity;
+    [~,stops] = size(destinations);  % gets number of stops
+    
+    % call data
+    fromY = call.fromFloor * config.FLOOR_HEIGHT; % we're comparing height, not floor number
+    
+    %% Scoring
+    % distanceFracBase
+    difference = abs(currentPos - fromY);
+    sums(iCar) = sums(iCar) + distanceFracBase*(1 - (difference/config.NUM_FLOORS));
+
+end
+
+%% determine best car,
+[~, idx] = max(sums);
+carIndex = idx;
+end
